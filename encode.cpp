9b74ffcf3cca1e0f6e5c6d5b7178c07e8d473978
Andrew Silkwood <andrewdavidsilkwood@gmail.com>
2020-09-28 14:14:57 -0700

    Debugged all found bugs with the insertion sort encoding algorithm

--- encode.cpp
+++ encode.cpp
@@ -4,7 +4,7 @@
 void encode(std::string keyword);
 
 std::string leftShift(std::string str, int shift);
-int insertionSort(std::string * stringPointers[], int size);
+void insertionSort(std::string * stringPointers[], int size);
 bool stringGT(std::string * str1, std::string * str2);
 
 int main(int argc, char *argv[]) {
@@ -17,19 +17,24 @@ int main(int argc, char *argv[]) {
 // main encoding method
 void encode(std::string keyword) {
     // checks for valid keyword
-    if (keyword == "insertion" || keyword == "quick") {
+    if (keyword == "insertion") {
+        bool addNewline = false;
+        std::string line;
+
         // encodes lines until it reaches end of input
-        while (std::cin.eof() == 0) {
+        while (!std::cin.eof()) {
+            // adds new line for empty strings; added to start of loop to avoid two newlines at end of file
+            if(addNewline) {
+                std::cout << std::endl;
+                addNewline = false;
+            }
+
             // receive line
-            std::string line;
             getline(std::cin, line);
-            // remove new line chars
-            if (std::cin.eof() == 0) { line = line.substr(0, line.length() - 1); }
 
             // encode line
-            if (line.length() == 0) {
-                // received blank line
-                std::cout << std::endl;
+            if (line.empty()) {
+                addNewline = true;
             } else {
                 // create array of shifted string pointers
                 int size = line.length();
@@ -43,21 +48,29 @@ void encode(std::string keyword) {
                 }
 
                 // sort pointers in lexicographic order according to ascii values
-                int startIx;    // index of the original first string
                 if (keyword == "insertion") {
                     // sort using insertion sort
-                    startIx = insertionSort(pointers, size);
-                } else {
+                    insertionSort(pointers, size);
+                } else if (keyword == "quick") {
                     // sorts using quick sort
-                    startIx = insertionSort(pointers, size - 1 + 1); // quick sort is unimplemented
+                    insertionSort(pointers, size - 1 + 1);
                 }
 
                 // output line 1 of encoding
-                // index in the sorted strings of the original first string the array
+                // index of the original string read in the sorted array
+                int startIx = -1;
+                int ix = 0;
+                while(startIx == -1 && ix < size) {
+                    if(line == *pointers[ix]) {
+                        startIx = ix;
+                    }
+                    ix++;
+                }
+                // -1 means string was not found
                 std::cout << startIx << std::endl;
 
                 // output line 2 of encoding
-                // clusters of ending characters "<count> <char>"
+                // clusters of the ending characters from each of the strings "<count> <char>"
                 char currChar = (*pointers[0]).at(size - 1);
                 int count = 0;
                 for (int i = 0; i < size; i++) {
@@ -79,52 +92,55 @@ void encode(std::string keyword) {
     }
 }
 
+// shifts string to the left
 std::string leftShift(std::string str, int shift) {
     std::string newStr = std::string(str);
     int size = str.length();
+
+    // offsets the new start of the string by the shift value
     for(int i = 0; i < size - shift; i++) {
         newStr[i] = str[shift + i];
     }
+    // adds cut off characters to the end of the string
     for(int i = size - shift; i < size; i++) {
         newStr[i] = str[i - size + shift];
     }
+
     return newStr;
 }
 
 // sorts string pointers in lexicographic order using insertion sort
-// returns the index of where the first string was originally
-int insertionSort(std::string * stringPointers[], int size) {
+void insertionSort(std::string * stringPointers[], int size) {
     std::string *key;
-    int firstIx = 0;    // first strings new index
     int ix;
+
+    // sorting algorithm
     for (int i = 1; i < size; i++) {
         key = stringPointers[i];
         ix = i - 1;
-        // insert string into already sorted strings
+        // inserts the next string into the already sorted strings
         while (ix >= 0 && stringGT(stringPointers[ix], key)) {
             stringPointers[ix + 1] = stringPointers[ix];
             ix--;
         }
         stringPointers[ix + 1] = key;
-        // increment index if key was placed before the first string
-        if (ix + 1 <= firstIx) {
-            firstIx++;
-        }
     }
-
-    return firstIx;
 }
 
 // tests if str1 is greater than str2 based on lexicographical order
+// assumes strings are equal lengths
 bool stringGT(std::string * str1, std::string * str2) {
     int ix = 0;
-    // checks through each character until on strings are unequal
+    // checks through each character until strings are unequal
     while(ix < (*str1).length()) {
         if((*str1)[ix] > (*str2)[ix]) {
+            // str1 was greater than str2
             return true;
         } else if((*str1)[ix] < (*str2)[ix]) {
+            // str1 was less than str2
             return false;
         }
+        // check next character
         ix++;
     }
     // strings were equal
